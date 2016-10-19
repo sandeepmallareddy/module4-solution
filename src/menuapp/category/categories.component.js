b5188d5af4b7af50d77ce03ba5d3f24e6da5dd93
Sandeep Mallareddy <msandeep85@gmail.com>
2016-10-19 23:51:34 +0530

    testing the retrieval of categories-9

--- src/menuapp/category/categories.component.js
+++ src/menuapp/category/categories.component.js
@@ -2,7 +2,7 @@
     angular.module('MenuApp')
 	.component('category',{
 	    templateUrl: 'src/menuapp/category/category.template.html',
-	    binding   : {
+	    bindings   : {
 		categories : '<',
 	    }
 	});
