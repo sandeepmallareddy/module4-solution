058abedbbb0bfbcb8d008fb47fd570fe98ec9310
Sandeep Mallareddy <msandeep85@gmail.com>
2016-10-20 00:40:07 +0530

    Testing Items component -5

--- src/menuapp/items/items.component.js
+++ src/menuapp/items/items.component.js
@@ -3,7 +3,7 @@
 
     angular.module('MenuApp')
 	.component('item',{
-	    templateUrl : '/src/menuapp/items/items.template.html',
+	    templateUrl : 'src/menuapp/items/items.template.html',
 	    bindings    : {
 		'items' : '<',
 	    },
